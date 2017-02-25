6f0c2a0dec7841e5d555a6850ba9e042232ddf0e
Robson Fernandes <robson.ux@gmail.com>
2017-02-25 00:19:51 -0300

    Life Care
    Desenvolvimento do Projeto

---
+++ lifeCare/src/pages/hello-ionic/hello-ionic.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'page-hello-ionic',
+  templateUrl: 'hello-ionic.html'
+})
+export class HelloIonicPage {
+
+  classificacao: string = "e";
+
+  constructor() {
+
+  }
+}
