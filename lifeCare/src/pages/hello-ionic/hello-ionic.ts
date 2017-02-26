439240e45cad008f2cd14f294fed351eb9e468cc
Robson Fernandes <robson.ux@gmail.com>
2017-02-25 22:28:40 -0300

    Alterações de Layout

---
+++ lifeCare/src/pages/hello-ionic/hello-ionic.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'page-hello-ionic',
+  templateUrl: 'hello-ionic.html'
+})
+export class HelloIonicPage {
+
+  classificacao: string = "e";
+  clinica: string = "a";
+
+  constructor() {
+
+  }
+}
